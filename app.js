d314083ea9bb0944095c40b82d4e997a5cd0114c
Deepmarik <lukyanovateam@gmail.com>
2021-01-31 22:45:49 +0300

    created app.js

---
+++ app.js
@@ -0,0 +1,16 @@
+"use strict";
+function buttonClick() {
+    ///let elem=document.getElementById("btn");
+    let elem1 = document.getElementById("btn1");
+    btn1.data = "img/cart.svg";
+
+}
+function go(elem) {
+    let elem1 = document.getElementById("btn1");
+    elem1.data = "img/cart-2.svg"
+}
+function stop(elem) {
+    let elem1 = document.getElementById("btn1");
+    elem1.data = "img/cart.svg"
+
+}
