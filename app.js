17fac6628a68ffd8f500889c983f23312448b81e
Deepmarik <lukyanovateam@gmail.com>
2021-02-04 16:01:15 +0300

    added addEventListener for cart

--- app.js
+++ app.js
@@ -1,16 +1,34 @@
 "use strict";
-function buttonClick() {
-    ///let elem=document.getElementById("btn");
-    let elem1 = document.getElementById("btn1");
-    btn1.data = "img/cart.svg";
-
-}
-function go(elem) {
-    let elem1 = document.getElementById("btn1");
+/* function go(elem) {
+    let elem1 = document.getElementById("cartButton");
     elem1.data = "img/cart-2.svg"
 }
-function stop(elem) {
-    let elem1 = document.getElementById("btn1");
+function stoped(elem) {
+    let elem1 = document.getElementById("cartButton");
     elem1.data = "img/cart.svg"
 
+}
+function down(elem) {
+    let elem1 = document.getElementById("cartButton");
+    elem1.data = "img/cart-3.svg"
+
+}
+let elem1 = document.getElementById("cartButton");
+elem1.addEventListener("click", down);
+elem1.addEventListener("mouseover", go);
+elem1.addEventListener("mouseout", stoped); */
+
+
+const eventSources = {
+    click: 'img/cart-3.svg',
+    mouseover: 'img/cart-2.svg',
+    mouseout: 'img/cart.svg'
+}
+
+let cartButton = document.getElementById('cartButton');
+
+for (let [event, source] of Object.entries(eventSources)) {
+    cartButton.addEventListener(event, (element) => {
+        element.target.src = source;
+    });
 }
